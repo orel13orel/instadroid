bd7202ba114a1692c02e8097a8368a6588fea888
Unknown <orel13orel@cs.colman.ac.il>
2019-08-22 19:08:44 +0300

    activity start

--- app/src/main/java/com/example/instadroid/StartActivity.java
+++ app/src/main/java/com/example/instadroid/StartActivity.java
@@ -9,6 +9,6 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
     }
 }
