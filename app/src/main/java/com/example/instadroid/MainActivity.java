eb64ec52d7daa4f9e6f1628535a634cdebf23cba
Unknown <orel13orel@cs.colman.ac.il>
2019-08-23 19:23:15 +0300

    finish bottom nav

--- app/src/main/java/com/example/instadroid/MainActivity.java
+++ app/src/main/java/com/example/instadroid/MainActivity.java
@@ -4,15 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.instadroid.Fragment.HomeFragment;
+import com.example.instadroid.Fragment.NotificationFragment;
+import com.example.instadroid.Fragment.ProfileFragment;
+import com.example.instadroid.Fragment.SearchFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
-    Fragment selectFragment=null;
+    Fragment selectedFragment=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,21 +29,40 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView=findViewById(R.id.bottom_navigation);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
 
     private  BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()){
-                case R.id.nav_home: break;
-                case R.id.nav_sea: break;
-                case R.id.nav_home: break;
-                case R.id.nav_home: break;
-                case R.id.nav_home: break;
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment=new HomeFragment();
+                    break;
+                case R.id.nav_search:
+                    selectedFragment=new SearchFragment();
+                    break;
+                case R.id.nav_add:
+                    startActivity(new Intent(MainActivity.this,PostActivity.class));
+                    break;
+                case R.id.nav_heart:
+                    selectedFragment=new NotificationFragment();
+                    break;
+                case R.id.nav_profile:
+                    SharedPreferences.Editor editor=getSharedPreferences("PREFS",MODE_PRIVATE).edit();
+                    editor.putString("profileid", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                    editor.apply();
+                    selectedFragment=new ProfileFragment();
+                    break;
+            }
+
+            if(selectedFragment!=null)
+            {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
             }
 
             return true;
         }
-    }
+    };
 }
