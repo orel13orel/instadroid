19ae66c5413d0348c61651c48fa2fc95610e7fc4
Unknown <orel13orel@cs.colman.ac.il>
2019-08-23 05:20:09 +0300

    new firebase and finish auth

--- app/src/main/java/com/example/instadroid/RegisterActivity.java
+++ app/src/main/java/com/example/instadroid/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.instadroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -13,8 +14,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -61,12 +69,45 @@ public class RegisterActivity extends AppCompatActivity {
                     Toast.makeText(RegisterActivity.this,"all filds r required",Toast.LENGTH_SHORT).show();
                 }else if(str_password.length()<6){
                     Toast.makeText(RegisterActivity.this,"pass too short",Toast.LENGTH_SHORT).show();
-                }else if
+                }else {
+                    register(str_username,str_fullname,str_email,str_password);
+                }
             }
         });
     }
-    private void register(String username, String fullname,String email,String password){
-        auth.createUserWithEmailAndPassword(email,password)
-                .addOnCanceledListener(RegisterActivity.this,new MediaPlayer.OnCompletionListener())
+    private void register(final String username, final String fullname, String email, String password){
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser firebaseUser = auth.getCurrentUser();
+                    String userid = firebaseUser.getUid();
+
+                    reference = FirebaseDatabase.getInstance().getReference().child("Users").child(userid);
+
+                    HashMap<String, Object> hashMap = new HashMap<>();
+                    hashMap.put("id", userid);
+                    hashMap.put("username", username.toLowerCase());
+                    hashMap.put("fullname", fullname);
+                    hashMap.put("bio", "");
+                    hashMap.put("imageurl", "https://firebasestorage.googleapis.com/v0/b/insta-33966.appspot.com/o/noprofile.png?alt=media&token=ac05042d-2f88-4047-adeb-b646747e6dd9");
+
+                    reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                pd.dismiss();
+                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+                }else {
+                    pd.dismiss();
+                    Toast.makeText(RegisterActivity.this,"try diffrent email or pass",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
