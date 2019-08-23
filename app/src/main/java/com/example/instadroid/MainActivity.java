4377306e33cd5ded73a84094f227aee6b74492bb
Unknown <orel13orel@cs.colman.ac.il>
2019-08-23 17:38:22 +0300

    start bottom nav

--- app/src/main/java/com/example/instadroid/MainActivity.java
+++ app/src/main/java/com/example/instadroid/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.instadroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Fragment selectFragment=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+    }
+
+    private  BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.nav_home: break;
+                case R.id.nav_sea: break;
+                case R.id.nav_home: break;
+                case R.id.nav_home: break;
+                case R.id.nav_home: break;
+            }
+
+            return true;
+        }
     }
 }
