b06795d4378608c4fb8eee5a4f60677fca106677
Unknown <orel13orel@cs.colman.ac.il>
2019-08-25 15:16:33 +0300

    fix posting

--- app/src/main/java/com/example/instadroid/PostActivity.java
+++ app/src/main/java/com/example/instadroid/PostActivity.java
@@ -30,66 +30,73 @@ import com.theartofdev.edmodo.cropper.CropImage;
 
 import java.util.HashMap;
 
+
+import com.google.firebase.storage.UploadTask;
+
+
 public class PostActivity extends AppCompatActivity {
 
-    Uri imageUri;
-    String myUrl="";
-    StorageTask uploadTask;
-    StorageReference storageReference;
+    private Uri mImageUri;
+    String miUrlOk = "";
+    private StorageTask uploadTask;
+    StorageReference storageRef;
 
-    ImageView close,image_added;
+    ImageView close, image_added;
     TextView post;
     EditText description;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
-        close=findViewById(R.id.close);
-        image_added=findViewById(R.id.image_added);
-        post=findViewById(R.id.post);
-        description=findViewById(R.id.description);
+        close = findViewById(R.id.close);
+        image_added = findViewById(R.id.image_added);
+        post = findViewById(R.id.post);
+        description = findViewById(R.id.description);
 
-        storageReference= FirebaseStorage.getInstance().getReference("posts");
+        storageRef = FirebaseStorage.getInstance().getReference("posts");
 
         close.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(PostActivity.this,MainActivity.class));
+            public void onClick(View view) {
+                startActivity(new Intent(PostActivity.this, MainActivity.class));
                 finish();
             }
         });
 
         post.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                uploadImage();
+            public void onClick(View view) {
+                uploadImage_10();
             }
         });
-        CropImage.activity().setAspectRatio(1,1).start(PostActivity.this);
+
+
+        CropImage.activity()
+                .setAspectRatio(1,1)
+                .start(PostActivity.this);
     }
 
     private String getFileExtension(Uri uri){
-        ContentResolver contentResolver=getContentResolver();
-        MimeTypeMap mime=MimeTypeMap.getSingleton();
-        return  mime.getExtensionFromMimeType(contentResolver.getType(uri));
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
     }
 
-    private void uploadImage(){
-        final ProgressDialog progressDialog=new ProgressDialog(this);
-        progressDialog.setMessage("posting");
-        progressDialog.show();
+    private void uploadImage_10(){
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setMessage("Posting");
+        pd.show();
+        if (mImageUri != null){
+            final StorageReference fileReference = storageRef.child(System.currentTimeMillis()
+                    + "." + getFileExtension(mImageUri));
 
-        if(imageUri!=null){
-            final StorageReference fileReference=storageReference.child(System.currentTimeMillis()+"."+getFileExtension(imageUri));
-
-            uploadTask =fileReference.putFile(imageUri);
-            uploadTask.continueWithTask(new Continuation() {
+            uploadTask = fileReference.putFile(mImageUri);
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
                 @Override
-                public Object then(@NonNull Task task) throws Exception {
-                    if(!task.isSuccessful()){
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()) {
                         throw task.getException();
                     }
                     return fileReference.getDownloadUrl();
@@ -97,50 +104,56 @@ public class PostActivity extends AppCompatActivity {
             }).addOnCompleteListener(new OnCompleteListener<Uri>() {
                 @Override
                 public void onComplete(@NonNull Task<Uri> task) {
-                    if(task.isSuccessful()){
-                        Uri downloadUri=task.getResult();
-                        myUrl=downloadUri.toString();
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+                        miUrlOk = downloadUri.toString();
 
-                        DatabaseReference reference= FirebaseDatabase.getInstance().getReference("Posts");
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
 
-                        String postid=reference.push().getKey();
+                        String postid = reference.push().getKey();
 
-                        HashMap<String,Object> hashMap=new HashMap<>();
-                        hashMap.put("postid",postid);
-                        hashMap.put("postimage",myUrl);
-                        hashMap.put("description",description.getText().toString());
+                        HashMap<String, Object> hashMap = new HashMap<>();
+                        hashMap.put("postid", postid);
+                        hashMap.put("postimage", miUrlOk);
+                        hashMap.put("description", description.getText().toString());
                         hashMap.put("publisher", FirebaseAuth.getInstance().getCurrentUser().getUid());
+
                         reference.child(postid).setValue(hashMap);
-                        progressDialog.dismiss();
-                        startActivity(new Intent(PostActivity.this,MainActivity.class));
+
+                        pd.dismiss();
+
+                        startActivity(new Intent(PostActivity.this, MainActivity.class));
                         finish();
-                    }else{
-                        Toast.makeText(PostActivity.this, "Failed to upload", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(PostActivity.this, "Failed", Toast.LENGTH_SHORT).show();
                     }
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(PostActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(PostActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
-        }else{
-            Toast.makeText(this, "pls select image", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(PostActivity.this, "No image selected", Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        //note: error!
-        if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && requestCode==RESULT_OK){
-            CropImage.ActivityResult result=CropImage.getActivityResult(data);
-            imageUri=result.getUri();
 
-            image_added.setImageURI(imageUri);
-        }else{
-            Toast.makeText(this, "error!", Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(PostActivity.this,MainActivity.class));
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            mImageUri = result.getUri();
+
+            image_added.setImageURI(mImageUri);
+        } else {
+            Toast.makeText(this, "Something gone wrong!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(PostActivity.this, MainActivity.class));
             finish();
         }
     }
